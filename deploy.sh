bafb67451c4c1a10baf2782f483dde8625533cb7
Ihor <ihor@hivepress.io>
2022-02-09 19:39:02 +0200

    Add hook generation

--- deploy.sh
+++ deploy.sh
@@ -174,6 +174,7 @@ run_build() {
     output 2 "Generating API docs..."
     echo
     ./vendor/bin/phpdoc run --template="data/templates/${project_name}" --sourcecode --defaultpackagename=${default_package}
+    php generate-hook-docs.php
 }
 
 main() {
